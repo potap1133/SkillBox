e0081c02c3585ed94529c24516b0bc382aed0d8a
Potapov <potapov83338@gmail.com>
2021-05-21 09:48:31 +0300

    Tryagle paskal

--- 04/HomeWork_4_01/HomeWork_4_01/Program.cs
+++ 04/HomeWork_4_01/HomeWork_4_01/Program.cs
@@ -10,11 +10,13 @@ namespace HomeWork_4_01
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+                                                                                                                            // Интцтализация переменных
             double j = 0;
+            double o = 0;
             double result = 0;
             double number_K = 0;
-
+            int l = 4;
+            double counter = 0;
 
             double number_N_Minus_K_Factorial = 0;
             double number_N_Factorial = 0;
@@ -24,25 +26,28 @@ namespace HomeWork_4_01
             double number_K_FactorialSecond = 1;
             double number_N_FactorialSecond = 1;
 
-            Console.Write($" Введите число : ");            // Просьба ввести число
-            double number_N = double.Parse(Console.ReadLine());   // Считываем введеное число
+            Console.Write($" Введите число : ");                                                                            // Просьба ввести число
+            double number_N = double.Parse(Console.ReadLine());                                                             // Считываем введеное число
 
             double t = number_N;
-            double[,] sizeOfMasiv = new double[(int)number_N +20000,(int)t + 20000 ];
+            double[,] sizeOfMasiv = new double[(int)number_N ,(int)t +1 ];
 
-            for (double i = 0; i <= number_N; i++)        //Открываем цикл вычисления бинома Ньютона.
+            for (double i = 0; i <= number_N; i++)                                                                          //Открываем цикл вычисления бинома Ньютона.
             {
-                number_N_Factorial = i;                   // Присваивание значения переменной
+                o = 10;
+                counter = number_N - (i * 4);
 
-                if ((number_N_Factorial == 0) | (number_N_Factorial == 1) ) // Условие для расчета
-                {                                                           // факториала числа "N"
-                    number_N_Factorial = 1;               //Присваивание значения переменной
+                number_N_Factorial = i;                                                                                     // Присваивание значения переменной
+
+                if ((number_N_Factorial == 0) | (number_N_Factorial == 1) )                                                 // Условие для расчета
+                {                                                                                                           // факториала числа "N"
+                    number_N_Factorial = 1;                                                                                 //Присваивание значения переменной
                 }
-                else if (number_N_Factorial <= number_N) // Условие для расчета факториала числа "N"
+                else if (number_N_Factorial <= number_N)                                                                    // Условие для расчета факториала числа "N"
                 {
-                    number_N_FactorialSecond = 1;        // Приведение переменной к исходному значению
+                    number_N_FactorialSecond = 1;                                                                           // Приведение переменной к исходному значению
 
-                    for (double a = 2; a <= i; a++)      // Вычисление факториала числа "N"
+                    for (double a = 2; a <= i; a++)                                                                         // Вычисление факториала числа "N"
                     {
 
                         number_N_FactorialSecond *= a;
@@ -51,24 +56,24 @@ namespace HomeWork_4_01
                 }
 
                 
-                number_K_FactorialSecond = 1;           // Приведение переменной к исходному значению
+                number_K_FactorialSecond = 1;                                                                               // Приведение переменной к исходному значению
 
-                number_N_Minus_K_FactorialSecond = 1;   // Приведение переменной к исходному значению
+                number_N_Minus_K_FactorialSecond = 1;                                                                       // Приведение переменной к исходному значению
 
-                for ( j = 0; j <= i; j++)               // Цикл вычисления факториалов чисел:
-                {                                       // "K" и "(N - K)"
-                   
-                    number_K_Factorial = j;             // Присваивание значения числу "K"
+                for ( j = 0; j <= i; j++)                                                                                   // Цикл вычисления факториалов чисел:
+                {                                                                                                           // "K" и "(N - K)"
+                    o++;
+                    number_K_Factorial = j;                                                                                 // Присваивание значения числу "K"
 
-                    if ((number_K_Factorial == 0) | (number_K_Factorial == 1)) // Условия вычисления
-                    {                                                          // факториала числа "K"
-                        number_K_Factorial = 1;         // Присвоение значения факториала числа "K"
+                    if ((number_K_Factorial == 0) | (number_K_Factorial == 1))                                              // Условия вычисления
+                    {                                                                                                       // факториала числа "K"
+                        number_K_Factorial = 1;                                                                             // Присвоение значения факториала числа "K"
                     }
-                    else if (number_K_Factorial <= i)   // Условия вычисления факториала числа "K"
+                    else if (number_K_Factorial <= i)                                                                       // Условия вычисления факториала числа "K"
                     {
-                        number_K_Factorial = 0;         // Обнуление значения числа "K"
-                        number_K_FactorialSecond = 1;   // Приведение переменной к исходному значению
-                        for (double b = 2; b <= j; b++) // Вычисление факториала числа "K"
+                        number_K_Factorial = 0;                                                                             // Обнуление значения числа "K"
+                        number_K_FactorialSecond = 1;                                                                       // Приведение переменной к исходному значению
+                        for (double b = 2; b <= j; b++)                                                                     // Вычисление факториала числа "K"
                         {
                             number_K_FactorialSecond *= b;
                             number_K_Factorial = number_K_FactorialSecond;
@@ -76,34 +81,38 @@ namespace HomeWork_4_01
                     }
 
                     
-                    number_N_Minus_K_Factorial = i - j;  // Вычисление значения разницы чисел "N" и "k"
+                    number_N_Minus_K_Factorial = i - j;                                                                     // Вычисление значения разницы чисел "N" и "k"
 
-                    if ((number_N_Minus_K_Factorial == 0) | (number_N_Minus_K_Factorial == 1)) // Условия вычисления
-                    {                                                                         // факториала разн. чисел
-                        number_N_Minus_K_Factorial = 1;  // Присваивание значения факториала разницы чисел "N" и "k"
+                    if ((number_N_Minus_K_Factorial == 0) | (number_N_Minus_K_Factorial == 1))                              // Условия вычисления
+                    {                                                                                                       // факториала разн. чисел
+                        number_N_Minus_K_Factorial = 1;                                                         // Присваивание значения факториала разницы чисел "N" и "k"
                     }
-                    else if (number_N_Minus_K_Factorial <= i)  // Условия вычисления факториала разницы чисел "N" и "k"
+                    else if (number_N_Minus_K_Factorial <= i)                                                   // Условия вычисления факториала разницы чисел "N" и "k"
                     {
-                        number_N_Minus_K_Factorial = 0;       // Обнуление значения разницы чисел "N" и "k
-                        number_N_Minus_K_FactorialSecond = 1; // Приведение переменной к исходному значению
+                        number_N_Minus_K_Factorial = 0;                                                                     // Обнуление значения разницы чисел "N" и "k
+                        number_N_Minus_K_FactorialSecond = 1;                                                               // Приведение переменной к исходному значению
 
-                        for (double z = 2; z <= (i - j); z++)  // Вычисление факториала разницы чисел "N" и "k
+                        for (double z = 2; z <= (i - j); z++)                                                               // Вычисление факториала разницы чисел "N" и "k
                         {
                             number_N_Minus_K_FactorialSecond *= z;
                             number_N_Minus_K_Factorial = number_N_Minus_K_FactorialSecond;
+                            
                         }
+                        
                     }
-
-                    // Итоговое вычисление бинома Ньютона
+                   
+                                                                                                                            // Итоговое вычисление бинома Ньютона
                     result = number_N_Factorial / (number_K_Factorial * number_N_Minus_K_Factorial);
 
                     //Console.SetCursorPosition((Console.WindowWidth - sizeOfMasiv.Length) / 2, Console.WindowHeight + (int)i);
 
-                    sizeOfMasiv[(int)i,(int)j] = (int)result;      // Присваивание значений элементам массива.
-                    Console.Write($" {sizeOfMasiv[(int)i, (int)j]} ");  // Вывод в консоль.
+                    
+                    //sizeOfMasiv[(int)i,(int)j] = (int)result;                                                             // Присваивание значений элементам массива.
+                    Console.SetCursorPosition((int)(counter + (8 * o)), (int)(i + 2));  
+                    Console.Write($" {(int)(result)/*sizeOfMasiv[(int)i, (int)j*/} ");                                      // Вывод в консоль.
                 }
-                Console.WriteLine();                              // Пропуск строки 
-                continue;                                         // Возвращение к началу решения
+                
+                continue;                                                                                                   // Возвращение к началу решения
             }
 
             Console.ReadKey();
---
+++ 04/HomeWork_4_01/HomeWork_4_01_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HomeWork_4_01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                                                                                                        // Инициализация переменных
+            var b = 1;
+            var counter = 0;
+            var valumeChanges = 0;
+            var numberTrianglePaskal = 0;
+            
+            Console.Write("Введите число : ");                                                                          // Запрос на введение числа 
+            numberTrianglePaskal = int.Parse(Console.ReadLine());                                                       // Считывание числа
+
+            int[,] arrayTriangle = new int[numberTrianglePaskal + 5, numberTrianglePaskal + 5];                         // Инициализация массива 
+
+            for (int i = 0; i <= numberTrianglePaskal; i++)                                                             // Цикл расчета элементов треугольника Паскаля
+            {
+                b = 10;
+                counter = numberTrianglePaskal - (i * 4);
+
+                for (int a = 0; a <= i; a++)
+                {
+                    b++;
+
+                    if ((a == 0) | (a == i))
+                    {
+                        valumeChanges = 1;
+                    }
+                    else
+                    {
+                        valumeChanges = arrayTriangle[i - 1, a - 1] + arrayTriangle[i - 1, a ];
+                        
+                    }
+
+                    Console.SetCursorPosition(counter + (8 * b) , i + 2);                                                   // Место положения курсора
+
+                    Console.Write($"{arrayTriangle[i, a] = valumeChanges} ");                                               // Вывод элементов треугольника
+                }
+                
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
